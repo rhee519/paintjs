53ff3195673cc3c1ad5f5e8e11ebf598e3c42942
Jiho Rhee <rhee519@gmail.com>
2021-07-10 13:27:02 +0900

    210710 stroke

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
- 
+const canvas = document.querySelector("#jsCanvas");
+const context = canvas.getContext("2d");
+const range = document.querySelector(".controls__range>input");
+
+canvas.width = 700;
+canvas.height = 700;
+context.strokeStyle = "#2c2c2c";
+context.lineWidth = parseFloat(range.value);
+
+let isPainting = false;
+
+if(canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
+    canvas.addEventListener("mouseleave", stopPainting);
+    range.addEventListener("change", onRangeChange);
+}
+
+function stopPainting() {
+    isPainting = false;
+}
+
+function startPainting() {
+    isPainting = true;
+}
+
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    if(!isPainting) {
+        context.beginPath();
+        context.moveTo(x, y);
+    } else {
+        context.lineTo(x, y);
+        context.stroke();
+    }
+}
+
+function onMouseDown(event) {
+    isPainting = true;
+}
+
+function onRangeChange(event) {
+    context.lineWidth = parseFloat(range.value);
+    console.log(context.lineWidth);
+}
